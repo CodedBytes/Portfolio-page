0758a78bd54bc95fbe05e61f2c1490dcb33c38b2
João Victor <137921917+CodedBytes@users.noreply.github.com>
2023-12-17 11:27:09 -0300

    feat: new language

--- src/app/langs-section/langs-section.component.ts
+++ src/app/langs-section/langs-section.component.ts
@@ -28,17 +28,18 @@ export class LangsSectionComponent {
   langs: Items[] = [
     new Items('https://api.iconify.design/devicon:html5.svg', 100, 'HTML5'),
     new Items('https://api.iconify.design/devicon:css3.svg', 100, 'CSS3'),
-    new Items('https://api.iconify.design/devicon:javascript.svg', 80, 'JavaScript'),
-    new Items('https://api.iconify.design/devicon:typescript.svg', 60, 'TypeScript'),
-    new Items('https://api.iconify.design/devicon:angular.svg', 30, 'Angular'),
+    new Items('https://api.iconify.design/vscode-icons:file-type-sass.svg', 50, 'SASS'),
+    new Items('https://api.iconify.design/devicon:javascript.svg', 85, 'JavaScript'),
+    new Items('https://api.iconify.design/devicon:typescript.svg', 65, 'TypeScript'),
+    new Items('https://api.iconify.design/devicon:angular.svg', 50, 'Angular'),
     new Items('https://api.iconify.design/devicon:php.svg', 50, 'PHP'),
     new Items('https://api.iconify.design/devicon:csharp.svg', 70, 'CSharp')
   ]
 
   // Lista de linguagens
   Databases: Items[] = [
-    new Items('https://api.iconify.design/tabler:brand-mysql.svg', 80, 'MySQL'),
-    new Items('https://api.iconify.design/simple-icons:microsoftsqlserver.svg', 70, 'MSSQL Server'),
+    new Items('https://api.iconify.design/tabler:brand-mysql.svg', 85, 'MySQL'),
+    new Items('https://api.iconify.design/simple-icons:microsoftsqlserver.svg', 75, 'MSSQL Server'),
   ]
 
   constructor(){}
